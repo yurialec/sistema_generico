e80879984cc912f6e6c5d78ae6a459e6e620c992
Yuri Alec <yuri.alec@hotmail.com>
2024-08-12 16:45:08 -0300

    melhorias css, graficos como exemplo

--- resources/js/components/Admin/Theme/HomeComponent.vue
+++ resources/js/components/Admin/Theme/HomeComponent.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="container" style="margin-bottom: 25px;">
-        <div class="row">
+    <div class="container">
+        <div class="row mb-3">
             <div class="col text-center">
                 <div class="card">
                     <div class="card-header">
                         grafico1
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Lorem Ipsum</h5>
+                        <h1>350</h1>
                     </div>
                 </div>
             </div>
@@ -17,21 +17,22 @@
                         grafico2
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Lorem Ipsum</h5>
+                        <h1>R$ 3.569,55</h1>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container" style="margin-bottom: 25px;">
-        <div class="row">
+
+        <div class="row mb-3">
             <div class="col text-center">
                 <div class="card">
                     <div class="card-header">
                         grafico3
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Lorem Ipsum</h5>
+                        <div class="chart-container">
+                            <canvas ref="chart3"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,22 +42,24 @@
                         grafico4
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Lorem Ipsum</h5>
+                        <div class="chart-container">
+                            <canvas ref="chart4"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="container">
-        <div class="row">
+        <div class="row mb-3">
             <div class="col text-center">
                 <div class="card">
                     <div class="card-header">
-                        grafico3
+                        grafico5
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Lorem Ipsum</h5>
+                        <div class="chart-container">
+                            <canvas ref="chart5"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -64,8 +67,113 @@
     </div>
 </template>
 
-<script>
-export default {
 
-}
+<script>
+import { Chart } from 'chart.js/auto';
+
+export default {
+    mounted() {
+        this.createChart('chart1', {
+            labels: ['January', 'February', 'March', 'April', 'May'],
+            datasets: [
+                {
+                    label: 'Sales',
+                    data: [30, 50, 40, 60, 70],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        }, 'bar');
+
+        this.createChart('chart2', {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+            datasets: [
+                {
+                    label: 'Votes',
+                    data: [12, 19, 3, 5, 2],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        }, 'bar');
+
+        this.createChart('chart3', {
+            labels: ['January', 'February', 'March', 'April', 'May'],
+            datasets: [
+                {
+                    label: 'Revenue',
+                    data: [100, 200, 150, 300, 250],
+                    backgroundColor: [
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: 'rgba(153, 102, 255, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        }, 'pie');
+
+        this.createChart('chart4', {
+            labels: ['A', 'B', 'C', 'D', 'E'],
+            datasets: [
+                {
+                    label: 'Performance',
+                    data: [70, 60, 80, 90, 85],
+                    backgroundColor: [
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                    ],
+                    borderColor: 'rgba(255, 206, 86, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        }, 'pie');
+
+        this.createChart('chart5', {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [
+                {
+                    label: 'Monthly Data',
+                    data: [120, 150, 170, 130, 180, 140, 160, 190, 200, 170, 210, 220],
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 2,
+                    fill: false,
+                },
+            ],
+        }, 'line');
+    },
+    methods: {
+        createChart(refName, chartData, chartType) {
+            new Chart(this.$refs[refName], {
+                type: chartType,
+                data: chartData,
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                },
+            });
+        },
+    },
+};
 </script>
