27404cb22caa8d9af3a74c1b5090e251179131a6
Yuri Alec <yuri.alec@hotmail.com>
2024-09-03 09:28:26 -0300

    melhorias landing page

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -90,7 +90,17 @@
                 <!-- Carousel Section -->
                 <div class="container carousel-container">
                     <div class="d-flex justify-content-center my-5">
-                        <div id="carouselExampleIndicators" class="carousel slide" style="max-width: 800px;">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true" style="max-width: 800px;">
+                            <div class="carousel-indicators">
+                                @foreach ($carousels as $index => $carousel)
+                                <button style="background-color: #333;" type="button" data-bs-target="#carouselExampleIndicators"
+                                    data-bs-slide-to="{{ $index }}"
+                                    class="{{ $index === 0 ? 'active' : '' }}"
+                                    aria-current="{{ $index === 0 ? 'true' : 'false' }}"
+                                    aria-label="Slide {{ $index + 1 }}">
+                                </button>
+                                @endforeach
+                            </div>
                             <div class="carousel-inner">
                                 @foreach ($carousels as $index => $carousel)
                                 <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
@@ -101,10 +111,8 @@
                                     <div class="carousel-caption d-md-block">
                                         <h5>{{ $carousel->title }}</h5>
                                         <p>{{ $carousel->description }}</p>
-                                        <a href="{{ $carousel->url_link }}"
-                                            target="_blank">{{ $carousel->name_link }}</a>
+                                        <a href="{{ $carousel->url_link }}" target="_blank">{{ $carousel->name_link }}</a>
                                     </div>
-                                    @else
                                     @endif
                                 </div>
                                 @endforeach
@@ -123,85 +131,63 @@
                     </div>
                 </div>
                 @endif
+            </div>
 
+            <div class="container blog-section text-center">
+                <h2>Blog</h2>
+                <div class="row justify-content-center">
 
-                <div class="container blog-section text-center">
-                    <h2>Blog</h2>
-                    <div class="row justify-content-center">
-
-                        <div class="col-md-4 d-flex align-items-stretch">
-                            <div class="card blog-card mb-4">
-                                <img class="card-img-top" alt="thumbnail-blog"
-                                    src="https://i.pinimg.com/564x/22/2a/5b/222a5b172d9bb69c4c4780261fa2f494.jpg">
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-primary">View</button>
-                                        </div>
-                                        <small class="text-muted">9 mins</small>
+                    <div class="col-md-4 d-flex align-items-stretch">
+                        <div class="card blog-card mb-4">
+                            <img class="card-img-top" alt="thumbnail-blog"
+                                src="https://i.pinimg.com/564x/22/2a/5b/222a5b172d9bb69c4c4780261fa2f494.jpg">
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-primary">View</button>
                                     </div>
+                                    <small class="text-muted">9 mins</small>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="col-md-4 d-flex align-items-stretch">
-                            <div class="card blog-card mb-4">
-                                <img class="card-img-top" alt="thumbnail-blog"
-                                    src="https://upload.wikimedia.org/wikipedia/commons/6/68/Szczenie_Jack_Russell_Terrier3.jpg">
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <button type="button"
-                                                class="btn btn-sm btn-outline-primary">View</button>
-                                        </div>
-                                        <small class="text-muted">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4 d-flex align-items-stretch">
-                            <div class="card blog-card mb-4">
-                                <img class="card-img-top" alt="thumbnail-blog"
-                                    src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <button type="button"
-                                                class="btn btn-sm btn-outline-primary">View</button>
-                                        </div>
-                                        <small class="text-muted">9 mins</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
-                </div>
 
-                <div class="container newsletter text-center py-5">
-                    <h2 class="display-4 font-weight-bold mb-4">Assine</h2>
-                    <p class="lead mb-4">Assine nossa newsletter e mantenha-se atualizado. Não se preocupe, não
-                        enviaremos
-                        muito conteúdo.</p>
-                    <form>
-                        <div class="input-group mb-3">
-                            <input type="email" class="form-control" placeholder="Insira o seu endereço de email"
-                                required>
-                            <button type="submit" class="btn btn-primary">Inscrever-se</button>
+                    <div class="col-md-4 d-flex align-items-stretch">
+                        <div class="card blog-card mb-4">
+                            <img class="card-img-top" alt="thumbnail-blog"
+                                src="https://upload.wikimedia.org/wikipedia/commons/6/68/Szczenie_Jack_Russell_Terrier3.jpg">
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button"
+                                            class="btn btn-sm btn-outline-primary">View</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
                         </div>
-                        {{-- <div class="form-check text-left">
-                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" required>
-                        <label class="form-check-label" for="defaultCheck1">
-                            Aceito a <a href="#">política de privacidade</a> e concordo em receber informações.
-                        </label>
-                    </div> --}}
-                    </form>
-                </div>
+                    </div>
 
+                    <div class="col-md-4 d-flex align-items-stretch">
+                        <div class="card blog-card mb-4">
+                            <img class="card-img-top" alt="thumbnail-blog"
+                                src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button"
+                                            class="btn btn-sm btn-outline-primary">View</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
     </div>
 
