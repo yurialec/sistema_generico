658f3105026826fcf4e33966673ecfaf5956a20e
Yuri Alec <yuri.alec@hotmail.com>
2024-10-16 14:24:51 -0300

    ajustes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'acl' => \App\Http\Middleware\AclMiddleware::class,
-        'redirect.client' => \App\Http\Middleware\RedirectIfClient::class,
     ];
 }
