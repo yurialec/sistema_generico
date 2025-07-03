fdab5185b78200b23396c684058d9db36fd1032f
Yuri Alec <yuri.alec@hotmail.com>
2025-07-03 15:55:36 -0300

    dsd

--- resources/js/Components/Admin/Theme/HomeComponent.vue
+++ resources/js/Components/Admin/Theme/HomeComponent.vue
@@ -1,8 +1,5 @@
 <template>
-    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-        <img height="400"
-            src="https://media.istockphoto.com/id/1413922045/pt/vetorial/programming-at-home.jpg?s=1024x1024&w=is&k=20&c=uw9AcXixi5Kc9sVrtX6z7RHfNAafnmmJtQXYh3f1LM8=">
-    </div>
+    
 </template>
 
 <script>
